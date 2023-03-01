62ee54444df8d3b7226a7f273db17ce1f4c01c23
Gobbera <leonardo.lgobbo@gmail.com>
2023-03-01 16:34:46 -0300

    close button

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-
+const chatButton = document.getElementById('chat-button');
+let closeButton = document.getElementById('close-button');
 let embbedChat = document.getElementById('embbed-chat');
 
-const chatButton = document.getElementById('chat-button');
-
+closeButton.hidden = true;
 embbedChat.hidden = true;
 
-chatButton.onclick = function() {
-  if (embbedChat.hidden == true) {
-    embbedChat.hidden = false;
-  } else {
-    embbedChat.hidden = true;
-  }
+chatButton.onclick = function () {
+  openChat();
 }
+closeButton.onclick = function() {
+  openChat();
+}
+
+function openChat() {
+    if (embbedChat.hidden == true) {
+      embbedChat.hidden = false;
+      closeButton.hidden = false;
+    } else {
+      embbedChat.hidden = true;
+      closeButton.hidden = true;
+    }
+}
+
