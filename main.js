90031e9980409c9497e61f9b2f332a5360813e9b
Gobbera <leonardo.lgobbo@gmail.com>
2023-03-03 17:55:16 -0300

    implementação de animação

--- main.js
+++ main.js
@@ -9,19 +9,25 @@ embbedChat.hidden = true;
 chatButton.onclick = function () {
   openChat();
 }
-closeButton.onclick = function() {
+closeButton.onclick = function () {
   openChat();
 }
 
 function openChat() {
-    if (embbedChat.hidden == true) {
-      embbedChat.hidden = false;
-      closeButton.hidden = false;
-      chatButton.hidden = true;
-    } else {
+  if (embbedChat.hidden == true) {
+    embbedChat.classList.add('openBox');
+    embbedChat.classList.remove('closeBox');
+    embbedChat.hidden = false;
+    closeButton.hidden = false;
+    chatButton.hidden = true;
+  } else {
+    embbedChat.classList.add('closeBox');
+    embbedChat.classList.remove('openBox');
+    setTimeout(function () {
       embbedChat.hidden = true;
       closeButton.hidden = true;
       chatButton.hidden = false;
-    }
+    }, 900);
+  }
 }
 
