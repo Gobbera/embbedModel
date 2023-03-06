5038a889e1391da6d4303104b5ae41a7398ec2ec
Gobbera <leonardo.lgobbo@gmail.com>
2023-03-06 15:34:22 -0300

    adaptação de embbed

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let chatButton = document.getElementById('chat-button');
 let closeButton = document.getElementById('close-button');
 let embbedChat = document.getElementById('embbed-chat');
 
-chatButton.hidden = false;
 closeButton.hidden = true;
 embbedChat.hidden = true;
 
@@ -19,14 +18,12 @@ function openChat() {
     embbedChat.classList.remove('closeBox');
     embbedChat.hidden = false;
     closeButton.hidden = false;
-    chatButton.hidden = true;
   } else {
     embbedChat.classList.add('closeBox');
     embbedChat.classList.remove('openBox');
     setTimeout(function () {
       embbedChat.hidden = true;
       closeButton.hidden = true;
-      chatButton.hidden = false;
     }, 900);
   }
 }
