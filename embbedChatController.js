92b2adc3c8789f29928d022496372b62d1e20b81
Gobbera <leonardo.lgobbo@gmail.com>
2023-03-06 17:14:11 -0300

    finalModel

---
+++ embbedChatController.js
@@ -0,0 +1,31 @@
+// embbed-chat JS configuration
+let chatButton = document.getElementById('chat-button');
+let closeButton = document.getElementById('close-button');
+let embbedChat = document.getElementById('embbed-chat');
+
+closeButton.hidden = true;
+embbedChat.hidden = true;
+
+chatButton.onclick = function () {
+  openChat();
+}
+closeButton.onclick = function () {
+  openChat();
+}
+
+function openChat() {
+  if (embbedChat.hidden == true) {
+    embbedChat.classList.add('openBox');
+    embbedChat.classList.remove('closeBox');
+    embbedChat.hidden = false;
+    closeButton.hidden = false;
+  } else {
+    embbedChat.classList.add('closeBox');
+    embbedChat.classList.remove('openBox');
+    setTimeout(function () {
+      embbedChat.hidden = true;
+      closeButton.hidden = true;
+    }, 900);
+  }
+}
+// embbed-chat JS configuration
